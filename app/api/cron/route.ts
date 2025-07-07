6f11a0e9fb62ed440bdc68d191b164b99049c4d7
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-07-07 18:59:46 -0400

    added a supasebase admin client (#24)
    * added a supasebase admin client
    
    * removed the headers from vercel.json

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,11 +1,16 @@
-import { createClient } from "@/utils/supabase/server";
-import { NextResponse } from "next/server";
+import { supabaseAdmin } from "@/utils/supabase/admin";
+import { NextRequest, NextResponse } from "next/server";
 
 
-export async function GET(){
-  const supabase = await createClient();
+export async function GET(req : NextRequest){
+  const authHeader = req.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new NextResponse('Unauthorized', {
+      status: 401,
+    });
+  }
 
-  const { error } = await supabase
+  const { error } = await supabaseAdmin
     .from("invoices")
     .update({ status : "Overdue" })
     .lt("due_date", new Date().toISOString())
