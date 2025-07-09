ad76afb6ec957692179cf11e98d7ad821a404112
Johnny Li <130239183+j0hnnyli@users.noreply.github.com>
2025-07-09 15:58:56 -0400

    styled the icon page for pdf

--- app/pdf/[id]/page.tsx
+++ app/pdf/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { getInvoice } from '@/lib/supabaseDataFns';
 import React from 'react';
 import PDFViewerComponent from '../../../components/pdf/PDFViewerComponent';
+import { FaExclamationTriangle } from "react-icons/fa";
+import Link from 'next/link';
+
 
 type InvoiceViewPageProps = {
   params : Promise<{id : number}>,
@@ -11,7 +14,22 @@ export default async function InvoiceViewPage({ params }: InvoiceViewPageProps) 
   const { data, error } = await getInvoice(id)
 
   if(!data || error){
-    return <p>Something Went Wrong!</p>
+    return (
+      <div className='h-screen w-full flex flex-col items-center justify-center text-white playfair'>
+        <div className="flex flex-col items-center space-x-4">
+          <FaExclamationTriangle className="text-6xl" />
+          <h1 className="text-3xl font-bold">Invoice Unavailable</h1>
+        </div>
+        <p className="mt-4 text-lg">Sorry, this invoice is no longer available</p>
+        <p className="text-lg">Please contact the sender directly</p>
+
+        <Link href='/'
+          className='py-2 px-4 bg-white/20 hover:bg-white/10 rounded-lg mt-6'
+        >
+          InvoTracker
+        </Link>
+      </div>
+    )
   }
 
 
